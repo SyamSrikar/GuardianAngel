ce126e24a1c4a774634d14b6b690a427ce24116d
syamsrikar <srampall1@gitam.in>
2023-05-07 20:16:45 +0530

    schedule change

--- src/Components/Schedule.jsx
+++ src/Components/Schedule.jsx
@@ -5,7 +5,7 @@ import '../home.css'
 const Schedule =(props) =>{
   return (
     <Flex flexDir={props.val?'column':'row'} backgroundColor={'#6B3273'}  alignItems={'center'} justifyContent={props.val?'space-evenly':'flex-start'}>
-        <Flex width={'90%'} padding={props.val?'10px 15px 10px':'30px 80px'} alignItems={'center'} flexDir={props.val && 'column'} height={props.val && '100%'} justifyContent={props.val && 'space-evenly'}>
+        <Flex width={'100%'} padding={props.val?'10px 15px 10px':'30px 80px'} alignItems={'center'} flexDir={props.val && 'column'} height={props.val && '100%'} justifyContent={props.val && 'space-evenly'}>
           <Flex className='home_container' flexDir={'column'} padding={props.val && '30px 0px'} >
               <p className={props.val?'container_div1 bold_font bold_font_mobile schedule':'container_div1 bold_font schedule'}>Request a Schedule For</p>
               <p className={props.val?'container_div2 bold_font bold_font_mobile schedule':'container_div2 bold_font schedule'}>Free Consultation</p>
